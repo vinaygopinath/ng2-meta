3ab55aad2447f821195ef66cc67943cc3a24201d
Vinay Gopinath <vinayg18@gmail.com>
2016-10-15 02:46:50 +0200

    Optional meta config properties
    Comments and type definitions

--- src/models/meta-config.ts
+++ src/models/meta-config.ts
@@ -1,4 +1,22 @@
 export interface MetaConfig {
-  useTitleSuffix: boolean;
-  defaults: {};
+  /**
+   * Flag to append an optional title suffix to the title.
+   * Default value: false
+   */
+  useTitleSuffix?: boolean;
+  /**
+   * A dictionary of default meta tags and their values
+   */
+  defaults?: {
+    /**
+     * The default title, used when a route does not have its own titleSuffix.
+     */
+    title?: string;
+    /**
+     * The default titleSuffix, used when useTitleSuffix is set to true
+     * and a route does not have its own titleSuffix.
+     */
+    titleSuffix?: string;
+    [key: string]: string;
+  };
 }
