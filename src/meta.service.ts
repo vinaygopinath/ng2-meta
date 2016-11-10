8c09341b8d6d5b5ca417e9f3ddb1884f893814fc
Michael Marriott <michaelm@fedgroup.co.za>
2016-11-10 21:12:36 +0200

    This checks to see if the tags have already been changed before trying to set the defaults for a page. With complex routing, one sometimes get the defaults overwriting whats been already set in the page. By checking the url, one can check if the tags/title have already been set for the page.

--- src/meta.service.ts
+++ src/meta.service.ts
@@ -11,87 +11,86 @@ const isDefined = (val: any) => typeof val !== 'undefined';
 
 @Injectable()
 export class MetaService {
-   private url:string;
+    private url: string;
 
-  constructor(private router: Router, @Inject(DOCUMENT) private document: any, private titleService: Title, private activatedRoute: ActivatedRoute, 
-              @Inject(META_CONFIG) private metaConfig: MetaConfig) {
-    this.router.events
-      .filter(event => (event instanceof NavigationEnd))
-      .map(() => (this.activatedRoute && this.activatedRoute.firstChild && this.activatedRoute.firstChild.snapshot && this.activatedRoute.firstChild.snapshot.data))
-      .subscribe((routeData: any) => {
-        this._updateMetaTags(routeData.meta);
-      });
-  }
-
-  private _getOrCreateMetaTag(name: string): HTMLElement {
-    let el: HTMLElement = this.document.querySelector(`meta[name='${name}']`);
-    if (!el) {
-      el = this.document.createElement('meta');
-      el.setAttribute('name', name);
-      this.document.head.appendChild(el);
-    }
-    return el;
-  }
-
-  private notLoaded(): boolean {
-      return this.url != this.document.location.href;
-  }
-
-  private _updateMetaTags(meta: any = {}) {
-
-    if (meta.disableUpdate) {
-      return false;
+    constructor(private router: Router, @Inject(DOCUMENT) private document: any, private titleService: Title, private activatedRoute: ActivatedRoute,
+        @Inject(META_CONFIG) private metaConfig: MetaConfig) {
+        this.router.events
+            .filter(event => (event instanceof NavigationEnd))
+            .map(() => (this.activatedRoute && this.activatedRoute.firstChild && this.activatedRoute.firstChild.snapshot && this.activatedRoute.firstChild.snapshot.data))
+            .subscribe((routeData: any) => {
+                this._updateMetaTags(routeData.meta);
+            });
     }
 
-    if (this.notLoaded()) {
-      this.setTitle(meta.title, meta.titleSuffix);
+    private _getOrCreateMetaTag(name: string): HTMLElement {
+        let el: HTMLElement = this.document.querySelector(`meta[name='${name}']`);
+        if (!el) {
+            el = this.document.createElement('meta');
+            el.setAttribute('name', name);
+            this.document.head.appendChild(el);
+        }
+        return el;
     }
 
-    Object.keys(meta).forEach(key => {
-      if (key === 'title' || key === 'titleSuffix') {
-        return;
-      }
-      this.setTag(key, meta[key]);
-    });
+    private _notLoaded(): boolean {
+        return this.url != this.document.location.href;
+    }
 
-    if (this.notLoaded()) {
-        Object.keys(this.metaConfig.defaults).forEach(key => {
-            if (key in meta || key === 'title' || key === 'titleSuffix') {
+    private _updateMetaTags(meta: any = {}) {
+
+        if (meta.disableUpdate) {
+            return false;
+        }
+
+        if (this._notLoaded()) {
+            this.setTitle(meta.title, meta.titleSuffix);
+        }
+
+        Object.keys(meta).forEach(key => {
+            if (key === 'title' || key === 'titleSuffix') {
                 return;
             }
-            this.setTag(key, this.metaConfig.defaults[key]);
+            this.setTag(key, meta[key]);
         });
+        if (this._notLoaded()) {
+            Object.keys(this.metaConfig.defaults).forEach(key => {
+                if (key in meta || key === 'title' || key === 'titleSuffix') {
+                    return;
+                }
+                this.setTag(key, this.metaConfig.defaults[key]);
+            });
+        }
     }
-  }
 
-  setTitle(title?: string, titleSuffix?: string): MetaService {
-    const titleElement = this._getOrCreateMetaTag('title');
-    const ogTitleElement = this._getOrCreateMetaTag('og:title');
-    let titleStr = isDefined(title) ? title : (this.metaConfig.defaults['title'] || '');
-    if (this.metaConfig.useTitleSuffix) {
-      titleStr += isDefined(titleSuffix) ? titleSuffix : (this.metaConfig.defaults['titleSuffix'] || '');
+    setTitle(title?: string, titleSuffix?: string): MetaService {
+        const titleElement = this._getOrCreateMetaTag('title');
+        const ogTitleElement = this._getOrCreateMetaTag('og:title');
+        let titleStr = isDefined(title) ? title : (this.metaConfig.defaults['title'] || '');
+        if (this.metaConfig.useTitleSuffix) {
+            titleStr += isDefined(titleSuffix) ? titleSuffix : (this.metaConfig.defaults['titleSuffix'] || '');
+        }
+
+        titleElement.setAttribute('content', titleStr);
+        ogTitleElement.setAttribute('content', titleStr);
+        this.titleService.setTitle(titleStr);
+        this.url = this.document.location.href;
+        return this;
     }
 
-    titleElement.setAttribute('content', titleStr);
-    ogTitleElement.setAttribute('content', titleStr);
-    this.titleService.setTitle(titleStr);
-    this.url = this.document.location.href;
-    return this;
-  }
-
-  setTag(tag: string, value: string): MetaService {
-    if (tag === 'title' || tag === 'titleSuffix') {
-      throw new Error(`Attempt to set ${tag} through 'setTag': 'title' and 'titleSuffix' are reserved tag names.
+    setTag(tag: string, value: string): MetaService {
+        if (tag === 'title' || tag === 'titleSuffix') {
+            throw new Error(`Attempt to set ${tag} through 'setTag': 'title' and 'titleSuffix' are reserved tag names.
       Please use 'MetaService.setTitle' instead`);
+        }
+        const tagElement = this._getOrCreateMetaTag(tag);
+        let tagStr = isDefined(value) ? value : (this.metaConfig.defaults[tag] || '');
+        tagElement.setAttribute('content', tagStr);
+        if (tag === 'description') {
+            let ogDescElement = this._getOrCreateMetaTag('og:description');
+            ogDescElement.setAttribute('content', tagStr);
+        }
+        this.url = this.document.location.href;
+        return this;
     }
-    const tagElement = this._getOrCreateMetaTag(tag);
-    let tagStr = isDefined(value) ? value : (this.metaConfig.defaults[tag] || '');
-    tagElement.setAttribute('content', tagStr);
-    if (tag === 'description') {
-      let ogDescElement = this._getOrCreateMetaTag('og:description');
-      ogDescElement.setAttribute('content', tagStr);
-    }
-    this.url = this.document.href;
-    return this;
-  }
 }
